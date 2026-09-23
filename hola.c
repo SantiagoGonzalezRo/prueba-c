ae27e749659f1b8faadf258de2ab00eea5db8c9f
Santiago Gonzalez <santiago.gonzalez.ro80@gmail.com>
2026-09-23 19:21:03 -0300

    agregue archivo hola.c

---
+++ hola.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int suma, numero;
+    printf("Ingresa un numero: \n");
+    scanf("%i", &numero);
+
+    suma = numero * 2;
+
+    printf("El numero duplicado es: %i\n", suma);
+
+    return 0;
+}
